acbb3d5f8948904ee1f757bc1a0a9c5b137c8b7a
hipnos2542 <hipnos2542@gmail.com>
2019-11-07 20:11:08 +0700

    Exercise4
    ส่งงาน

---
+++ Exercise4_Phumin_P.py
@@ -0,0 +1,10 @@
+SF = float(10)
+SG = float(20)
+SI = float(30)
+SC = float(50)
+
+print("---Your Score---")
+print("Foundation English               :", SF)
+print("General Business                 :", SG)
+print("Introduction to Computer Systems :", SI)
+print("Computer Programming             :", SC)
