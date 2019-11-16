37c92312477c448dd6fd9216986785351b133f32
hipnos2542 <hipnos2542@gmail.com>
2019-11-16 19:18:15 +0700

    Create Exercise5_1_Phumin_P.py
    ส่งงาน 5-1

---
+++ Exercise5_1_Phumin_P.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number  : "))
+num2 = int(input("Enter second number :"))
+print(str(num1) + " + " + str(num2) + " = " + str(num1+num2))
+print(str(num1) + " - " + str(num2) + " = " + str(num1-num2))
+print(str(num1) + " * " + str(num2) + " = " + str(num1*num2))
+print(str(num1) + " / " + str(num2) + " = " + str(int(num1/num2)))
