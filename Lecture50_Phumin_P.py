818d3b36fbd4715b6303286317296fd8fbb6da79
hipnos2542 <hipnos2542@gmail.com>
2019-11-28 11:42:43 +0700

    Create Lecture50_Phumin_P.py

---
+++ Lecture50_Phumin_P.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def subNumber(x,y):
+    print(x-y)
+def mulNumber(x,y):
+    print(x*y)
+def divNumber(x,y):
+    print(x/y)
+addNumber(5,3)
+subNumber(5,3)
+mulNumber(5,3)
+divNumber(5,3)
