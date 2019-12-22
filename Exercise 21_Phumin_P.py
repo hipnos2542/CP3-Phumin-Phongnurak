a0ed64659a9ccf246651fc71b6c17f3581c10c56
hipnos2542 <hipnos2542@gmail.com>
2019-12-22 10:14:28 +0700

    ส่งงาน

--- Exercise 21_Phumin_P.py
+++ Exercise 21_Phumin_P.py
@@ -1,21 +1,32 @@
 from tkinter import *
-def BMIresult(BMI):# ผลลัพธ์หลังจากคำนวน
-    if BMI <= 18.5:
-        result.configure(text="Too thin")
-    elif (BMI > 18.5) and BMI <= 23.0:
-        result.configure(text="Normal weight")
-    elif (BMI > 23.0) and BMI < 24.0:
-        result.configure(text="Overweight")
-    elif (BMI >= 24) and BMI < 30.0:
-        result.configure(text="Fat")
-    else:
-        result.configure(text="Very fat")
 
 def leftClickButton(Event):
+    def BMIresult(BMI):  # ผลลัพธ์หลังจากคำนวน
+        if BMI <= 18.5:
+            result.configure(text="Too thin")
+        elif (BMI > 18.5) and BMI <= 23.0:
+            result.configure(text="Normal weight")
+        elif (BMI > 23.0) and BMI < 24.0:
+            result.configure(text="Overweight")
+        elif (BMI >= 24) and BMI < 30.0:
+            result.configure(text="Fat")
+        else:
+            result.configure(text="Very fat")
+
+    windowTwo = Tk()
+    windowTwo.title("Result Calculate")
+    windowTwo.geometry("200x40")
+    Label(windowTwo, text="BMI : ").grid(row=0, column=0)
+    result = Label(windowTwo, text="Result")
+    result.grid(row=1, column=1)
+    resultNumber = Label(windowTwo, text="Number Result")
+    resultNumber.grid(row=0, column=1)
     weight = float(textBox2.get())
     height = float(textBox.get())/100
     BMI = weight/(height**2)
+    resultNumber.configure(text="%.2f" %(BMI))
     BMIresult(BMI)
+    windowTwo.mainloop()
 
 mainWindow = Tk()
 
@@ -31,10 +42,7 @@ textBox2 = Entry(mainWindow)
 textBox2.grid(row=1,column=1)
 
 calculateButton = Button(mainWindow, text="Calculate BMI")
-calculateButton.grid(row=2, column=0)
+calculateButton.grid(row=2, column=1)
 calculateButton.bind("<Button-1>", leftClickButton)
-
-result = Label(mainWindow, text=" Result")
-result.grid(row=2, column=1)
 
 mainWindow.mainloop()
